5b204111a02449ad5999042f572049070990d709
Dan Dart <git@dandart.co.uk>
2021-08-03 00:03:41 +0100

    some more

--- src/Main.hs
+++ src/Main.hs
@@ -1,35 +1,17 @@
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 
+-- import Data.Either
 import Data.Gedcom
-import Data.Gedcom.Internal.CoreTypes
 import qualified Data.Text as T
-
-data Known a = Never | Unknown | Known a deriving (Eq, Show)
-
-data Person = Person {
-    name :: Text,
-    born :: Maybe UTCTime,
-    died :: Known UTCTime,
-    location :: Text,
-    occupation :: Text
-} deriving (Eq, Show)
-
--- Tree type?
-data Pedigree = Pedigree {
-    person :: Person,
-    mother :: Maybe Pedigree,
-    father :: Maybe Pedigree
-} deriving (Eq, Show)
-
-gedcomToPedigree :: Gedcom -> Pedigree
-gedcomToPedigree = undefined
 
 main :: IO ()
 main = do
     Right (gedcom, xref) <- parseGedcomFile "familytree.ged"
-    let firstPerson = (\(GDStructure a) -> a) $ head $ gedcomIndividual gedcom
-    let firstPersonName = (\(Name a b) -> a) $ personalNameName $ (\(Just a) -> a) $ individualName $ firstPerson
+    let firstPerson = (\(Right a) -> a) $ flip gdLookup xref $ head $ gedcomIndividual gedcom
+    -- let family = rights $ flip gdLookup xref <$> gedcomFamily gedcom
+    let personsName = (\(Name a b) -> a) . personalNameName . (\(Just a) -> a) . individualName
+    let firstPersonName = personsName firstPerson
     let number = length $ gedcomIndividual gedcom
-    putStrLn $ "First person named: " <> T.unpack firstPerson
+    putStrLn $ "First person named: " <> T.unpack firstPersonName
     putStrLn $ "Number of individuals: " <> show number
     pure ()
