d644874b265b19a2cdd23bb3340f386e345ec798
Dan Dart <git@dandart.co.uk>
2023-09-09 15:52:20 +0100

    fix build again

--- default.nix
+++ default.nix
@@ -12,6 +12,16 @@ let
   myHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: rec {
       family = lib.dontHaddock (self.callCabal2nix "family" (gitignore ./.) {});
+      # text-all = lib.doJailbreak (lib.markUnbroken super.text-all);
+      # gedcom = lib.doJailbreak (super.gedcom);
+
+      # Release to cabal not yet made
+      gedcom = lib.doJailbreak (self.callCabal2nix "gedcom" (nixpkgs.fetchFromGitHub {
+        owner = "CLowcay";
+        repo = "hs-gedcom";
+        rev = "148acdf9664d234d9ec67121448b92d786aa4461";
+        sha256 = "Q6bycMXI4a9ZP5J/Cok4WN/ynjoWGybvwWrAy9Za/ag=";
+      }) {});
     };
   };
   shell = myHaskellPackages.shellFor {
