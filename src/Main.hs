d5787d4ce46fdf9a7ecf6d2cfce2499229d51231
Dan Dart <git@dandart.co.uk>
2025-01-24 21:33:48 +0000

    updates

--- src/Main.hs
+++ src/Main.hs
@@ -1,26 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unused-matches -Wno-x-partial #-}
 
 module Main (main) where
 
 import Control.Monad
 import Control.Monad.IO.Class
-import Control.Monad.Trans.Except
+import Control.Monad.Except
 import Data.Gedcom
 import Data.Gedcom.Structure
 import Data.Maybe
 import Data.Text                  qualified as T
+import Data.Text.IO               qualified as TIO
 
 main ∷ IO ()
 main = do
     result <- parseGedcomFile "familytree.ged"
     case result of
         Right (gedcom, xref) -> void . runExceptT $ do
-            firstPerson <- except . flip gdLookup xref . head $ gedcomIndividual gedcom
+            firstPerson <- liftEither . flip gdLookup xref . head $ gedcomIndividual gedcom
             -- let family = rights $ flip gdLookup xref <$> gedcomFamily gedcom
             let personsName = (\(Name a b) -> a) . personalNameName . fromJust . individualName
             let firstPersonName = personsName firstPerson
             let number = length $ gedcomIndividual gedcom
-            liftIO . putStrLn $ "First person named: " <> T.unpack firstPersonName
-            liftIO . putStrLn $ "Number of individuals: " <> show number
+            liftIO . TIO.putStrLn $ "First person named: " <> firstPersonName
+            liftIO . TIO.putStrLn $ "Number of individuals: " <> T.show number
             pure ()
         _ -> error "Failure"
